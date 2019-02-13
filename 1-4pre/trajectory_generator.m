01018556430c426141f9ff6e10e7e123c6e13fa7
Ariella Mansfield <ariellamansfield@gmail.com>
2019-02-13 17:51:59 -0500

    slope velocity trajectory

--- 1-4pre/trajectory_generator.m
+++ 1-4pre/trajectory_generator.m
@@ -27,11 +27,12 @@ end
 
 desired_state = [];
 
-time2goal=40;
+time2goal=20;
 [numSteps,~]=size(path);
 numSteps=numSteps-1;
 timePerStep=time2goal/numSteps;
 
+% path 
 if t>time2goal
     x_des=path(end,1);
     y_des=path(end,2);
@@ -43,27 +44,47 @@ if t>time2goal
     y_acc=0;
     z_acc=0;
 else
-    index=ceil(t/timePerStep+1e-10);
-    nextIndex=index+1;
-    x_0=path(index,1);
-    y_0=path(index,2);
-    z_0=path(index,3);
-    t_0=(index-1)*timePerStep;
-    x_vel=(path(nextIndex,1)-path(index,1))/timePerStep;
-    y_vel=(path(nextIndex,2)-path(index,2))/timePerStep;
-    z_vel=(path(nextIndex,3)-path(index,3))/timePerStep; 
-    x_des=x_vel*(t-t_0)+x_0;
-    y_des=y_vel*(t-t_0)+y_0;
-    z_des=z_vel*(t-t_0)+z_0;
-    x_acc=0;
-    y_acc=0;
-    z_acc=0;
+    currIndex=ceil(t/timePerStep+1e-10);
+    nextIndex=currIndex+1;
+    x_0=path(currIndex,1);
+    y_0=path(currIndex,2);
+    z_0=path(currIndex,3);
+    t_0=(currIndex-1)*timePerStep;
+    t_2=timePerStep+t_0;
+    t_1=t_0+(t_2-t_0)/2;
+    slopeVel_x=(path(nextIndex,1)-path(currIndex,1))/(t_1^2-t_0*t_1+t_0^2/2+t_2^2/2-t_1*t_2);
+    slopeVel_y=(path(nextIndex,2)-path(currIndex,2))/(t_1^2-t_0*t_1+t_0^2/2+t_2^2/2-t_1*t_2);
+    slopeVel_z=(path(nextIndex,3)-path(currIndex,3))/(t_1^2-t_0*t_1+t_0^2/2+t_2^2/2-t_1*t_2);
+    if t<=t_1 %before the halfway point of this step
+        x_vel=slopeVel_x*t-slopeVel_x*t_0;
+        y_vel=slopeVel_y*t-slopeVel_y*t_0;
+        z_vel=slopeVel_z*t-slopeVel_z*t_0; 
+        x_des=slopeVel_x*(t^2/2-t_0*t+t_0^2/2)+x_0;
+        y_des=slopeVel_y*(t^2/2-t_0*t+t_0^2/2)+y_0;
+        z_des=slopeVel_z*(t^2/2-t_0*t+t_0^2/2)+z_0;
+        x_acc=slopeVel_x;
+        y_acc=slopeVel_y;
+        z_acc=slopeVel_z;
+    else
+        x_vel=-slopeVel_x*t+slopeVel_x*t_2;
+        y_vel=-slopeVel_y*t+slopeVel_y*t_2;
+        z_vel=-slopeVel_z*t+slopeVel_z*t_2; 
+        x_des=slopeVel_x*(t_1^2/2-t_0*t_1+t_0^2/2+t^2/2+t_1^2/2-t_1*t)+x_0;
+        y_des=slopeVel_y*(t_1^2/2-t_0*t_1+t_0^2/2+t^2/2+t_1^2/2-t_1*t)+y_0;
+        z_des=slopeVel_z*(t_1^2/2-t_0*t_1+t_0^2/2+t^2/2+t_1^2/2-t_1*t)+z_0;
+        x_acc=-slopeVel_x;
+        y_acc=-slopeVel_y;
+        z_acc=-slopeVel_z;
+    end
 end
 pos = [x_des; y_des; z_des];
 vel = [x_vel;y_vel; z_vel];
 acc = [x_acc;y_acc ; z_acc];
 yaw = 0;
 yawdot = 0;
+
+
+
 
 % use the "persistent" keyword to keep your trajectory around
 % inbetween function calls
